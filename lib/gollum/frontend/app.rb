923c4744e997143cb76a15b60cf60ab9eb895948
Sir Valiance <sir@sirvaliance.com>
2011-07-28 20:49:42 -0500

    Fixed /create redirect bug.

--- lib/gollum/frontend/app.rb
+++ lib/gollum/frontend/app.rb
@@ -70,7 +70,7 @@ module Precious
       update_wiki_page(wiki, page.sidebar, params[:sidebar], commit) if params[:sidebar]
       committer.commit
 
-      redirect "/#{CGI.escape(Gollum::Page.cname(name))}"
+      redirect "/page/#{CGI.escape(page.path)}"
     end
 
     post '/create' do
@@ -81,7 +81,7 @@ module Precious
 
       begin
         wiki.write_page(name, format, params[:content], commit_message)
-        redirect "/#{CGI.escape(name)}"
+        redirect "/page/#{CGI.escape(name)}"
       rescue Gollum::DuplicatePageError => e
         @message = "Duplicate page: #{e.message}"
         mustache :error
